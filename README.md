62e51ad81e017c5fbee7f5ff49eda5761e22236a
Connor Winning <connoredwinwinning2367@gmail.com>
2024-03-11 20:50:09 -0500

    Init

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Teensy-Driver-Controller
 Driver Controller Firmware for the Teensy 3.6 microcontroller written using the Arduino IDE
+
+
---
+++ can.h
@@ -0,0 +1,10 @@
+/*
+*  MST Solar Car CAN interface header
+*  
+*  Designed for use with the Teensy 3.6 Development board
+*  and for use with the Arduino IDE, with 
+*
+*
+*
+*
+*/
